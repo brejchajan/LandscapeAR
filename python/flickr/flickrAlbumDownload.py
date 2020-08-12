866efcf7823f5e57dd1b548379f76658ab2eab04
Jan Brejcha <brejchaja@gmail.com>
2020-08-12 11:48:58 +0200

    Updated license.

--- python/flickr/flickrAlbumDownload.py
+++ python/flickr/flickrAlbumDownload.py
@@ -5,6 +5,39 @@
 # @Email:  brejcha@adobe.com, ibrejcha@fit.vutbr.cz, brejchaja@gmail.com
 # @Project: ImmersiveTripReports 2017-2018
 # AdobePatentID="P7840-US"
+# @License: Copyright 2020 CPhoto@FIT, Brno University of Technology,
+# Faculty of Information Technology,
+# Božetěchova 2, 612 00, Brno, Czech Republic
+#
+# Redistribution and use in source code form, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions must retain the above copyright notice, this list of
+#    conditions and the following disclaimer.
+#
+# 2. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# 3. Redistributions must be pursued only for non-commercial research
+#    collaboration and demonstration purposes.
+#
+# 4. Where separate files retain their original licence terms
+#    (e.g. MPL 2.0, Apache licence), these licence terms are announced, prevail
+#    these terms and must be complied.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+# WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+# OF SUCH DAMAGE.
+
 #
 # Description
 # ============
--- python/flickr/getFlickrPhotosInfo.py
+++ python/flickr/getFlickrPhotosInfo.py
@@ -1,3 +1,45 @@
+# @Author: Jan Brejcha <ibrejcha>
+# @Date:   2020-08-04T11:51:50+02:00
+# @Email:  ibrejcha@fit.vutbr.cz; brejchaja@gmail.com
+# @Project: LandscapeAR
+# @Last modified by:   ibrejcha
+# @Last modified time: 2020-08-12T11:41:50+02:00
+# @License: Copyright 2020 CPhoto@FIT, Brno University of Technology,
+# Faculty of Information Technology,
+# Božetěchova 2, 612 00, Brno, Czech Republic
+#
+# Redistribution and use in source code form, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions must retain the above copyright notice, this list of
+#    conditions and the following disclaimer.
+#
+# 2. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# 3. Redistributions must be pursued only for non-commercial research
+#    collaboration and demonstration purposes.
+#
+# 4. Where separate files retain their original licence terms
+#    (e.g. MPL 2.0, Apache licence), these licence terms are announced, prevail
+#    these terms and must be complied.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+# WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+# OF SUCH DAMAGE.
+
+
+
+
 import argparse as ap
 import flickrapi
 from FlickrPrefs import FlickrPrefs
